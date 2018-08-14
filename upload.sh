dc36f6b534f1a322fcfccf8a7386f63e81b2fbf1
Max Kviatkouski <kvyatkovskij@gmail.com>
2018-08-13 21:24:18 -0700

    reverted upload script back to non-profiled version

--- upload.sh
+++ upload.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws --profile kviatkouski s3 cp . s3://mukilteo.pnwdiving.org/ --recursive --exclude ".git/*" --exclude ".idea/*"
+aws s3 cp . s3://mukilteo.pnwdiving.org/ --recursive --exclude ".git/*" --exclude ".idea/*"
